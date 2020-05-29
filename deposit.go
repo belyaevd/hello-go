5f0f04291b76ff17af42dfee865f67392cbb46c3
belyaevd <belyaevpilot@gmail.com>
2020-05-29 11:03:49 +0300

    add func and curkl

--- deposit.go
+++ deposit.go
@@ -5,18 +5,18 @@ import (
 	"os"
 )
 
-func main()  {
+func main() {
 	var sum float64
 	var rate float64
-	var res float64
 	print("Введите сумму вклада")
 	fmt.Fscan(os.Stdin, &sum)
 	print("Введите процент")
 	fmt.Fscan(os.Stdin, &rate)
-	for i := 0; i < 5; i++ {
-
+	fmt.Printf("Сумма вклада в  1 год: %.2f \n ", sum)
+	var sumAfter = sum
+	for i := 0; i < 6; i++ {
+		sumAfter = sumAfter + (sumAfter*rate)/100
+		fmt.Printf("Сумма вклада в ", i, " год: %.2f \n", sumAfter)
 
 	}
-
-
 }
